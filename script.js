4cb6d1001f6d623f3e6e5af46f03f3c2469f3fc5
jhon Doe <marianohernandez07@yahoo.com>
2025-07-27 11:25:50 -0500

    arregle js y hml file para poder arreglar 2nd tabla

--- script.js
+++ script.js
@@ -2,34 +2,26 @@
  
 //VARIABLES GLOBALES
 
-let row =null;
 function Enviar() {
     let dataEntered =retriveData();
     let readData = readingDataFromLocalstarage(dataEntered);
+   console.log(readData);
     insert(readData);
-    if (readData == false) {
-        msg.innerHTML = `<h3 style = "color: red;">Por favor, complete todos los campos.</h3>`;
     }
-    else { if (row ==null) {
-     
-        msg.innerHTML = "Data insertada!";
-    } else { update();
-
-        msg.innerHTML = "Datos enviados correctamente";}
-
-    }
+    
     document.getElementById("form").reset ();
-}    
+    
     //CREAR
-
+   //Funcion para recuperar los datos del formulario
     function retriveData() {
-    let nombreyapellidos = document.getElementById("Nombreyapellidos").value;
+    let nombreyapellidos = document.getElementById("Nombre y apellidos").value;
     let direccion = document.getElementById("Direccion").value;
     let curp = document.getElementById("Curp").value;
     let telefono = document.getElementById("Telefono").value;
-    let correo = document.getElementById("Correo").value;
+    let Email = document.getElementById("Email").value;
     
-    let array = [nombreyapellidos, direccion, curp, telefono, correo];
+    let array = [nombreyapellidos, direccion, curp, telefono, Email];
+    // va a regresar todos los valores que se ingresaron en el formulario
     return array;
     }
    //READ
@@ -37,26 +29,26 @@ function Enviar() {
     function readingDataFromLocalstarage(dataEntered) {
     
     //Data guardada en local storage
-         let n= localStorage.setItem("nombreyapellidos", dataEntered[0]);
-         let d= localStorage.setItem("direccion", dataEntered[1]);
-         let c= localStorage.setItem("curp", dataEntered[2]);
-         let t= localStorage.setItem("telefono", dataEntered[3]);
-         let k= localStorage.setItem("correo", dataEntered[4]);
+         let n = localStorage.setItem("nombre y apellidos", dataEntered[0]);
+         let d = localStorage.setItem("direccion", dataEntered[1]);
+         let c = localStorage.setItem("curp", dataEntered[2]);
+         let t = localStorage.setItem("telefono", dataEntered[3]);
+         let k = localStorage.setItem("Email", dataEntered[4]);
 
-            //getting valores de localstorage to table
+            //sacar valores de localstorage hacia la segunda tabla
 
             let n1 = localStorage.getItem("nombreyapellidos",n);
             let d1 = localStorage.getItem("direccion", d);
             let c1 = localStorage.getItem("curp" ,c);
             let t1 = localStorage.getItem("telefono",t);
-            let k1 = localStorage.getItem("correo",k);
+            let k1 = localStorage.getItem("Email",k);
 
             let array = [n1, d1, c1, t1, k1];
             return array;
            
     }
 
-    //INSERTAR
+    //INSERTAR para mostrar en la tabla de localstorage los valores que se inserten en la solicitud
     function insert(readData) {
        let row = table.insertRow(); 
        let cell1 = row.insertCell(0);
@@ -70,6 +62,7 @@ function Enviar() {
        cell3.innerHTML = readData[2];
        cell4.innerHTML = readData[3];
        cell5.innerHTML = readData[4];
+       
 
 
     }
